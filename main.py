58064e2f5c1fce8b1ba656f2d5878f0a14d5e728
Assil Ksiksi <cyph0nik@gmail.com>
2012-04-22 09:28:17 +0400

    Minor modifications and finalization.

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 # http://docs.python.org/library/time.html
 # https://code.google.com/p/python-twitter/
 # http://stackoverflow.com/a/4389549
-# http://wolfram.kriesing.de/blog/index.php/2006/learning-how-to-calculate-with-date-and-time-in-python
 # http://www.saltycrane.com/blog/2007/09/how-to-sort-python-dictionary-by-keys/
 # http://stackoverflow.com/questions/1345827/how-do-i-find-the-time-difference-between-two-datetime-objects-in-python
 
@@ -21,15 +20,16 @@ def scrape():
     page = etree.HTML(urllib2.urlopen("http://awqaf.ae/?Lang=EN").read())
 
     times = page.xpath("//div[contains(@class, 'wrap-4 alignLeft push-left-2')]//li/text()")[:6]
-    prayers = page.xpath("//div[contains(@class, 'wrap-4 alignLeft push-left-2')]//li//span/text()")[:6]
-    
+    prayers_en = page.xpath("//div[contains(@class, 'wrap-4 alignLeft push-left-2')]//li//span/text()")[:6]
+
     times.pop(1)
-    prayers.pop(1)
+    prayers_en.pop(1)
 
     salah = {}
 
-    for i, j in zip(prayers, times):
+    for i, j in zip(prayers_en, times):
         salah[i] = j
+
     for key, value in salah.iteritems():
         t_minus = datetime.strptime(value, '%I:%M %p') - timedelta(minutes=2) # Deduct 2 minutes to convert to Al Ain prayer time
         salah[key] = datetime.strftime(t_minus, '%H:%M') # Lovely time parsing using the built-in datetime module
@@ -42,8 +42,9 @@ def main():
     sleep = 0 # Set this to a default value
 
     print "Currently running. Please do not close this window.\n"
+    print "Wake up in {0} hours".format(sleep)
 
-    time.sleep(sleep) # Initial sleep
+    time.sleep(sleep * 3600) # Initial sleep
 
     while True:
         now = time.asctime(time.localtime())[11:16]
@@ -54,13 +55,14 @@ def main():
         for key, value in sorted(salah.iteritems(), key=lambda (key, value): (value, key)):
             if count == 1:
                 sleep = datetime.strptime(value, '%H:%M') - sleep
-                time.sleep(sleep.seconds - 120) # Exit sleep 2 minutes before next prayer
+                print "Wake up in {0} hours".format(sleep.seconds / 3600.0)
+                time.sleep(sleep.seconds - 120)
                 sleep, count = 0, 0
             if now == value:
-                api.PostUpdate("It is now time for {0} prayer in Al Ain.".format(key, value))
-                print "Tweeted {0} prayer and time.".format(key)
-                salah[key] = 'z'
+                api.PostUpdate("It is now time for {0} prayer in Al Ain [{1}].".format(key, value))
+                print "Tweeted {0} prayer and time. ({1} at {2})".format(key, time.asctime(time.localtime())[4:10], value)
                 sleep = datetime.strptime(value, '%H:%M')
+                salah[key] = 'z'
                 count += 1
 
 if __name__ == '__main__':
